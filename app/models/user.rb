e022d0ba36f19479de6c2ceb2a6f2ba5f5a5e170
limepie <akaton@gmail.com>
2024-10-01 15:55:21 +0300

    feat: Добавлен функционал создания и получения баджей

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :test_passages
   has_many :gists
+  has_many :user_badges
+  has_many :badges, through: :user_badges
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
---
+++ app/services/BadgeAwardService.rb
@@ -0,0 +1,43 @@
+class BadgeAwardService
+  def initialize(user, test_passage)
+    @user = user
+    @test_passage = test_passage
+    @badges = []
+  end
+
+  def call
+    Badge.all.each do |badge|
+      @badges << badge if send("check_#{badge.rule_type}?", badge)
+    end
+    @user.badges << @badges if @badges.present?
+  end
+
+  private
+
+  # Проверка на выполнение правила "Прохождение всех тестов из категории"
+  def check_category_complete?(badge)
+    category = Category.find_by(title: badge.rule_value)
+    return false unless category
+
+    tests_in_category = category.tests.ids
+    passed_tests_in_category = @user.test_passages.where(test_id: tests_in_category).select(&:success?).map(&:test_id).uniq
+
+    tests_in_category.sort == passed_tests_in_category.sort
+  end
+
+  # Проверка на правило "Прохождение теста с первой попытки"
+  def check_first_attempt?(badge)
+    @user.test_passages.where(test_id: @test_passage.test_id).count == 1 && @test_passage.success?
+  end
+
+  # Проверка на выполнение правила "Прохождение всех тестов определённого уровня"
+  def check_level_complete?(badge)
+    level = badge.rule_value.to_i
+    tests_of_level = Test.where(level: level).ids
+    passed_tests_of_level = @user.test_passages.where(test_id: tests_of_level).select(&:success?).map(&:test_id).uniq
+
+    tests_of_level.sort == passed_tests_of_level.sort
+  end
+end
+
+
